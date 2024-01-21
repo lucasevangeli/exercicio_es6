cc3d4a23d493cf94a1fc568828158e131a702383
Lucas Evan <lucasevan478@gmail.com>
2024-01-21 16:01:26 -0300

    nota_de_alunos

---
+++ src/teste.js
@@ -0,0 +1,21 @@
+
+const alunos = [
+    { nome: 'João', nota: 7 },
+    { nome: 'Maria', nota: 5 },
+    { nome: 'Pedro', nota: 8 },
+    { nome: 'Ana', nota: 6 },
+    { nome: 'Carlos', nota: 4 }
+  ];
+  
+  
+  function alunosAprovados(arrayAlunos) {
+    return arrayAlunos.filter(aluno => aluno.nota >= 6);
+  }
+  
+  
+  const alunosAprovadosArray = alunosAprovados(alunos);
+  
+
+  console.log('Alunos aprovados:');
+  console.log(alunosAprovadosArray);
+  
